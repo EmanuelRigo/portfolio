e48a4b168cd22b2bd12399980e8b4ee28d2e3baf
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-05-12 22:37:35 -0300

    commit

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   FaHtml5,
   FaCss3Alt,
@@ -11,6 +9,7 @@ import {
   FaDatabase,
   FaJs,
   FaLinux,
+  FaLanguage,
 } from "react-icons/fa";
 import {
   SiNextdotjs,
@@ -20,8 +19,14 @@ import {
   SiExpress,
   SiTypescript,
   SiDbeaver,
+  SiPostgresql,
 } from "react-icons/si";
 import { TbWorldWww } from "react-icons/tb";
+
+export const metadata = {
+  title: "Emanuel R. | Resume",
+  description: "Página de contacto para que puedas comunicarte conmigo.",
+};
 
 const skills = {
   frontend: [
@@ -39,6 +44,7 @@ const skills = {
     { name: "Express", icon: <SiExpress className="text-white" /> },
     { name: "MongoDB", icon: <SiMongodb className="text-green-500" /> },
     { name: "MySQL", icon: <SiMysql className="text-blue-500" /> },
+    { name: "PostgreSQL", icon: <SiPostgresql className="text-blue-700" /> },
   ],
   others: [
     { name: "Git", icon: <FaGitAlt className="text-red-500" /> },
@@ -50,15 +56,19 @@ const skills = {
     },
     { name: "Linux", icon: <FaLinux className="text-black" /> },
     { name: "DBeaver", icon: <SiDbeaver className="text-orange-900" /> },
+    { name: "Inglés B1", icon: <FaLanguage className="text-green-400" /> },
   ],
 };
 
 const ResumePage = () => {
   return (
-    <section className="px-6 text-neutral-100 h-full bg-neutral-900">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 h-full">
+    <section className=" text-neutral-100 h-full bg-neutral-900 ">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 h-full w-full md:px-6">
         {Object.entries(skills).map(([category, items]) => (
-          <div key={category} className="bg-neutral-800 rounded-xl p-6 h-full">
+          <div
+            key={category}
+            className="bg-neutral-800 rounded-xl p-6 h-full w-full"
+          >
             <h2 className="text-xl font-semibold mb-4 capitalize border-b border-neutral-600 pb-2">
               {category}
             </h2>
@@ -66,7 +76,7 @@ const ResumePage = () => {
               {items.map(({ name, icon }) => (
                 <li key={name} className="flex items-center gap-3">
                   <span>{icon}</span>
-                  <span>{name}</span>
+                  <span className="">{name}</span>
                 </li>
               ))}
             </ul>

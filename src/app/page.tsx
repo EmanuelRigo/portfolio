6adc9e26daaba1118cb409ce69ad76839e2d0def
EmanuelRigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-07-30 19:38:21 -0300

    adding button older projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,34 @@
+"use client";
+
+import { useState } from "react";
 import ProjectCard from "@/components/ProjectCard";
 import projectData from "@/data/projectData.json";
-import { Project } from "@/types/project"; // si usás archivo aparte
-
-export const metadata = {
-  title: "Emanuel R.",
-  description: "Página de contacto para que puedas comunicarte conmigo.",
-  keywords: [
-    "Emanuel Rigo",
-    "portafolio",
-    "desarrollador web",
-    "React",
-    "Next.js",
-    "Tailwind CSS",
-    "TypeScript",
-    "MongoDB",
-    "Node.js",
-  ],
-  openGraph: {
-    title: "Emanuel R. | Desarrollador Web",
-    description:
-      "Portafolio personal de Emanuel Rigo, desarrollador web fullstack.",
-    url: "https://portfolio-dusky-rho-64.vercel.app/m",
-    siteName: "Emanuel R. Portfolio",
-    images: [
-      {
-        url: "https://portfolio-dusky-rho-64.vercel.app/favicon.ico",
-        width: 1200,
-        height: 630,
-        alt: "Vista previa del portafolio de Emanuel R.",
-      },
-    ],
-    locale: "es_ES",
-    type: "website",
-  },
-};
+import oldProjectData from "@/data/oldProjectData.json";
+import { Project } from "@/types/project";
 
 const Page = () => {
-  const projects: Project[] = Object.values(projectData);
+  const [useOldProjects, setUseOldProjects] = useState(false);
+
+  const projects: Project[] = Object.values(
+    useOldProjects ? oldProjectData : projectData
+  );
 
   return (
-    <div className="grid md:grid-cols-[repeat(2,1fr)] grid-rows-[repeat(3,214px)]  gap-6 justify-items-stretch md:px-6 text-black animate-fadeIn">
-      {projects.map((project, index) => (
-        <ProjectCard key={index} {...project} />
-      ))}
+    <div className="flex flex-col gap-4 h-full ">
+      <div className="flex justify-end px-6 z-50 absolute -bottom-3 right-0">
+        <button
+          onClick={() => setUseOldProjects(!useOldProjects)}
+          className="px-2 py-1 bg-yellow-400 text-black font-semibold rounded-md hover:bg-yellow-500 transition-all text-sm"
+        >
+          {useOldProjects ? "Proyectos nuevos" : "Proyectos antiguos"}
+        </button>
+      </div>
+
+      <div className="grid md:grid-cols-[repeat(2,1fr)] auto-rows-[214px] gap-6 justify-items-stretch md:px-6 text-black animate-fadeIn flex-grow">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+      </div>
     </div>
   );
 };
